fa0cf982a0d999d788a61daa45e74e4914debbde
wiggapony0925 <ninjeff06@gmail.com>
2023-08-28 17:23:22 -0400

    Sucessfully scraped for stocks with most volume

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import requests
+from volume_stock import find_stocks
 
 
 def main():
     st.title("Stock Prediction Model")
 
-:
+    stocks = find_stocks()
+    st.multiselect("here are todays stocks", stocks)
 
 
 if __name__ == "__main__":
--- src/volume_stock.py
+++ src/volume_stock.py
@@ -6,17 +6,20 @@ def find_stocks():
     url = 'https://finance.yahoo.com/most-active/'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    
+
     most_volume_stocks = []
 
-    table = soup.find("table", {"class", "w(100%)"})
-    if table:
-        rows = table.find_all("tr")
-        for row in rows[1:]:  # Skip the header row
-            symbol_cell = row.find("td", {"aria-label": "Symbol"})
-            if symbol_cell:
-                symbol = symbol_cell.text
-                most_volume_stocks.append(symbol)
+    # Find all table rows with the specific class
+    rows = soup.find_all("tr", class_="simpTblRow")
+
+    for row in rows:
+        symbol_cell = row.find("a", {"data-test": "quoteLink"})
+        if symbol_cell:
+            symbol = symbol_cell.text.strip()
+            most_volume_stocks.append(symbol)
 
     return most_volume_stocks
 
+
+# Test the function
+print(find_stocks())
