58f58a1c1135adee33df116d32b46b3a63e85a34
wiggapony0925 <ninjeff06@gmail.com>
2023-09-02 21:45:41 -0400

    modernized application first draft where data display worked

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from volume_stock import find_stocks
 from stockInformation import stock_information
 from dataframe import fetch_stock_data, display_stock_data, date_range
+import config
+
 
 def main():
     st.set_page_config(
@@ -23,9 +25,14 @@ def main():
 
     with col1:
         st.markdown("### Select a Stock")
-        selected_stock = st.selectbox("Select a stock", stocks, index=stocks.index(st.session_state.selected_stock) if st.session_state.selected_stock else 0)
+        selected_stock = st.selectbox("Select a stock", stocks, index=stocks.index(
+            st.session_state.selected_stock) if st.session_state.selected_stock else 0)
         confirm_button = st.button("Confirm Stock")
         st.session_state.selected_stock = selected_stock if confirm_button else st.session_state.selected_stock
+        config.selected_stock = st.session_state.selected_stock
+
+        # Add a "Learn More" link
+        st.markdown("[ℹ️ Learn more about stock selection](#)")
 
     with col2:
         if st.session_state.selected_stock:
@@ -35,17 +42,51 @@ def main():
             st.markdown("### No Stock Selected")
 
     st.markdown("---")
+    st.markdown("## Stock Description")
 
+    stock_info = stock_information()
+    if st.session_state.selected_stock and stock_info is not None:
+        with st.spinner("Fetching stock information..."):
+            company_name = stock_info.get("Company Name", "N/A")
+            industry = stock_info.get("Industry", "N/A")
+            website = stock_info.get("Website", "N/A")
+            description = stock_info.get("Description", "N/A")
+
+            st.write(f"**Company Name:** {company_name}")
+            st.write(f"**Industry:** {industry}")
+            st.write(f"**Website:** [{website}]({website})")
+            st.write(f"**Description:** {description}")
+    elif st.session_state.selected_stock:
+        st.error("Stock information not available.")
+
+    # Add a separator
+    st.markdown("---")
+
+    # Add a date range picker with a default range
     selected_start_date, selected_end_date = date_range()
+    st.markdown("### Date Range")
+    with st.expander("Select a Date Range"):
+        selected_start_date = st.date_input("Start Date", selected_start_date)
+        selected_end_date = st.date_input("End Date", selected_end_date)
 
     # Fetch stock data based on selected date range
     stock_data = fetch_stock_data(st.session_state.selected_stock, selected_start_date, selected_end_date)
 
     # Display stock data using the display_stock_data function
-    display_stock_data(stock_data)
+    display_stock_data(st.session_state.selected_stock, selected_start_date, selected_end_date)
 
-    st.markdown("### Additional Features Coming Soon!")
-    st.markdown("Stay tuned for more interactive features and insights on your selected stock.")
+    # Add a separator
+    st.markdown("---")
+
+    # Add a button to show a popup with additional features
+    if st.button("🚀 Explore Additional Features"):
+        st.write("Stay tuned for more interactive features and insights on your selected stock.")
+        # Add more content or features in the future
+
+    # Add a footer with a "Feedback" link
+    st.markdown("<div style='text-align: center; margin-top: 20px;'>"
+                "<a href='#' style='color: #888;'>Provide Feedback</a></div>",
+                unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
--- src/dataframe.py
+++ src/dataframe.py
@@ -1,8 +1,7 @@
 import yfinance as yf
 import streamlit as st
 from datetime import datetime, timedelta
-from config import selected_stock, DEFAULT_START_DATE, DEFAULT_END_DATE
-
+from config import DEFAULT_START_DATE, DEFAULT_END_DATE
 
 def date_range():
     st.markdown("## Please choose a range of dates for the data frame")
@@ -11,7 +10,6 @@ def date_range():
     selected_end_date = st.date_input("Select end date", DEFAULT_END_DATE)
 
     return selected_start_date, selected_end_date
-
 
 def fetch_stock_data(selected_stock, start_date, end_date):
     if start_date is None or end_date is None:
@@ -32,14 +30,32 @@ def fetch_stock_data(selected_stock, start_date, end_date):
     return stock_data
 
 
-def display_stock_data(stock_data):
+def display_stock_data(selected_stock, selected_start_date, selected_end_date):
+    stock_data = fetch_stock_data(selected_stock, selected_start_date, selected_end_date)
+
     if stock_data is None:
         return
 
     st.markdown("## Stock Data")
 
-    st.write("Displaying data for selected date range:")
-    st.write(f"Start Date: {stock_data.index[0]}")
-    st.write(f"End Date: {stock_data.index[-1]}")
+    view_option = st.selectbox("Select View", ["Dataframe", "Line Chart", "Area Chart"])
 
-    st.line_chart(stock_data["Close"], use_container_width=True)
+    refresh_button = st.button("Refresh")
+
+    if refresh_button:
+        st.experimental_rerun()
+
+    st.write(f"**Company Name:** {selected_stock}")
+
+    if view_option == "Dataframe":
+        st.dataframe(stock_data)
+    elif view_option == "Line Chart":
+        st.line_chart(stock_data["Close"], use_container_width=True)
+    elif view_option == "Area Chart":
+        st.area_chart(stock_data["Close"], use_container_width=True)
+
+    st.markdown("Displaying data for selected date range:")
+    st.write(f"**Start Date:** {stock_data.index[0]}")
+    st.write(f"**End Date:** {stock_data.index[-1]}")
+
+    return stock_data
