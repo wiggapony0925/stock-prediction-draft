6e6a0638045e80e793f9ab68154a614f90814b87
wiggapony0925 <ninjeff06@gmail.com>
2023-09-18 21:23:27 -0400

    string to datetime

--- src/examplePrediction.py
+++ src/examplePrediction.py
@@ -3,5 +3,16 @@ import pandas as pd
 import datetime
 
 data = None #data would be data of selected stock
-
 df = pd.read_csv(data)
+
+
+def string_to_datetime(s):
+    split = s.split('-')
+    year, month, day = int(split[0]), int(split[1]), int(split[2])
+    return datetime.datetime(year=year, month=month, day=day)
+
+#testting
+
+datetime_object = None..
+
+#https://www.youtube.com/watch?v=CbTU92pbDKw   3:45
