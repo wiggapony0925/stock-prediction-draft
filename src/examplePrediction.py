c5913d09ceb120ae3423beb92b173ab97b159603
wiggapony0925 <ninjeff06@gmail.com>
2023-10-01 01:49:18 -0400

    added functionality to prediction file

--- src/examplePrediction.py
+++ src/examplePrediction.py
@@ -1,6 +1,25 @@
 from config import selected_stock
 import pandas as pd
+from datetime import date
 import datetime
-
 import yfinance as yf
-import streamlit as st
+
+
+from plotly import graph_objs as go
+
+Current_Date = datetime.date.today()
+Date = str(Current_Date)
+
+
+def load_data():
+    pass
+
+
+def plot_raw_data():
+    pass
+
+
+def predict_data():
+    pass
+
+#https://www.youtube.com/watch?v=0E_31WqVzCY&t=288s
